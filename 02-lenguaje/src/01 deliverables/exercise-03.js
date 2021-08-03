869d0d7476721cb66fd83f5f329abee199b62eec
Fran de Jesus Mazzoni <fran@lemoncode.net>
2021-08-03 14:19:41 +0200

    End languages exercices

--- 02-lenguaje/src/01 deliverables/exercise-03.js
+++ 02-lenguaje/src/01 deliverables/exercise-03.js
@@ -1,3 +1,9 @@
 console.log("************** DELIVERABLE 03 *********************");
 
 const clone = (a) => ({ ...a });
+const merge = (source, target) => ({ ...source, ...target });
+
+const a = { name: "Maria", surname: "Ibañez", country: "SPA" };
+const b = { name: "Luisa", age: 31, married: true };
+
+console.log("Merge", merge(a, b));
--- 02-lenguaje/src/01 deliverables/exercise-05.ts
+++ 02-lenguaje/src/01 deliverables/exercise-05.ts
@@ -1,1 +1,33 @@
 console.log("************** DELIVERABLE 05 *********************");
+
+class SlothMachine {
+  private cont = 0;
+
+  private getRandomBoolean = () => {
+    const rnd = Math.random();
+    return rnd > 0.5;
+  };
+
+  public play = () => {
+    this.cont++;
+    const value1 = this.getRandomBoolean();
+    const value2 = this.getRandomBoolean();
+    const value3 = this.getRandomBoolean();
+
+    let result = "Good luck next time!!";
+
+    if (value1 && value2 && value3) {
+      result = `Congratulations!!!. You won ${this.cont} coins!!`;
+      this.cont = 0;
+    }
+
+    console.log(result);
+  };
+}
+
+const machine1 = new SlothMachine();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
